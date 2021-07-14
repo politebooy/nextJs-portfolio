a73f98552ffe4572385a9458c8affaaba307e377
unknown <billahmukim@gmail.com>
2021-07-14 21:34:26 +0600

    committed

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,13 +1,22 @@
 import React from 'react'
 import Layout from '../components/1-Layout/Layout'
 import Section from '../components/4-Section/Section'
+import Head from 'next/head'
 
 export default function about() {
     return (
+        <>
+        
+      <Head>
+        <title>About pate title</title>
+        <meta name="description" content="About pate description....................." />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
         <Layout>
             <Section title="About Page">
                 <h4>this is about page</h4>
             </Section>
         </Layout>
+        </>
     )
 }
